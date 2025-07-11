ab81180c220132aafbea3fc1fe0875377e9af04a
Eduardo Barros <eduardocarneiro83@gmail.com>
2025-07-10 21:57:37 -0300

    Cadastro de Clientes OOP Version 0.2

--- 01. Fundamentos de Python/teste.py
+++ 01. Fundamentos de Python/teste.py
@@ -1,4 +1,7 @@
-my_name = input('Digite seu nome: ').title()
+# Função que mostra informação dos clientes na tela
+def display_clients(name, age, email, ltv):
+    print(f'Cliente: {name} | Idade: {age} | login: {email} | LTV: R$ {ltv:.2f}')
 
-print(my_name)
-print(type(my_name))
+display_clients("Mario", 55, 'itsmemario@gmail.com', 10577)
+
+
