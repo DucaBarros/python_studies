b889b57272b80c586c45b2096ec72a2c9bf1a55e
Eduardo Barros <eduardocarneiro83@gmail.com>
2025-07-23 21:56:25 -0300

    PMA untill class 09 & all files renamed

--- 02. Manipulação de Arquivos, PEP8 e Testes Simples com Python/test.py
+++ 02. Manipulação de Arquivos, PEP8 e Testes Simples com Python/test.py
@@ -1,18 +1,10 @@
+import pathlib
 
-# Classe List
-class List:                             # Criando a classe lista
-    def __init__(self, name):           # Definindo o método construtor da lista
-      self._filename = f'{name}.txt'    # Salvando o nome do arquivo em uma variável protegida
+# Creating files
 
-    def get_list(self):                             # Definindo a função para pegar a list
-        list = []                                   # Criando a lista
-        with open(self._filename, 'r') as file:     # Usando um métido para ler os itens da lista
-          for item in file:                         # Loop para verificar os itens da lista após a abertura
-            list.append(item.strip())               # Adicionando os itens na lista
-        return list                                 # Retorna a lista pronta
-    
+myfile = pathlib.Path('test.py')
 
-list = List('Lista')
-for item in list.get_list():
-   print(f'- {item}')
-
+if myfile.exists() == False:
+    print('Yes, I am here!')
+else:
+    print('Where am I?')
